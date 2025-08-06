19121ec3d1f836a5498ffc60ab178c36dff54bcf
TerryLeeTectonics <terrywaiholee@gmail.com>
2025-08-06 10:35:47 -0700

    Add batch mode and new arguments for 2D cross section query
    Added batch mode and new arguments for input model path and output file path.

--- ctm_plotting/query_2d_cross_section.py
+++ ctm_plotting/query_2d_cross_section.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import xarray as xr
+import argparse
 from pyproj import Geod
 
 # Import initiation functions
@@ -20,10 +21,10 @@ from write_csv_output import write_csv_output
 ## query the model along a vertical cross-section between lon/lat pairs
 #  - inputs: start longitude and latitude, end longitude and latitude, start and end depth, and model name; optional plotting
 #  - returns: DataFrame with temperature at these points; optional figure
-def query_2D_vertical_cross_section(lat_start, lon_start, lat_end, lon_end, z_start, z_end, modelname, plot=False):
+def query_2D_vertical_cross_section(lat_start, lon_start, lat_end, lon_end, z_start, z_end, modelname, modelpath, plot = False):
     
     # initialize dataset
-    xdata = init_ctm(modelname)
+    xdata = init_ctm(modelname, modelpath)
 
     # check validity of query
     check_inbounds_values(xdata, {"longitude[°]": [lon_start, lon_end], 
@@ -55,24 +56,38 @@ def query_2D_vertical_cross_section(lat_start, lon_start, lat_end, lon_end, z_st
     else:
         return df
 
-# Make sure the following is not calling when it is being imported, only runs the following when directly run at command prompt
-if __name__ == "__main__":
-    lat_start = float(input('Enter the starting latitude (°): '))
-    lon_start = float(input('Enter the starting longitude (°): '))
-    lat_end = float(input('Enter the ending latitude (°): '))
-    lon_end = float(input('Enter the ending longitude (°): '))
-    z_start = float(input('Enter the starting depth (m): '))
-    z_end = float(input('Enter the ending depth (m): '))
-    modelname = str(input('Enter model name (Lee_2025 or Shinevar_2018): '))
-
+# Make a function to allow batch mode
+def call_func():
+    
+    par = argparse.ArgumentParser()
+    par.add_argument('--lat_start', type = float, required = True)    # Add argument of starting latitude (°)
+    par.add_argument('--lon_start', type = float, required = True)    # Add argument of starting longitude (°)
+    par.add_argument('--lat_end', type = float, required = True)      # Add argument of ending latitude (°)
+    par.add_argument('--lon_end', type = float, required = True)      # Add argument of ending longitude (°)
+    par.add_argument('--z_start', type = float, required = True)      # Add arugment of starting depth (m)
+    par.add_argument('--z_end', type = float, required = True)        # Add arugment of ending depth (m)
+    par.add_argument('--modelname', type = str, required = True)      # Add argument of model name: Lee_2025 or Shinevar_2018
+    par.add_argument('--modelpath', type = str, required = True)      # Add argument of input model path
+    par.add_argument('--outpath', type = str, required = True)        # Add argument of output file path and file name
+    
+    args = par.parse_args()                                           # Extract arguments
+    
     # Call the function
     df = query_2D_vertical_cross_section(
-        lat_start,
-        lon_start,
-        lat_end,
-        lon_end,
-        z_start,
-        z_end,
-        modelname)
+         args.lat_start,
+         args.lon_start,
+         args.lat_end,
+         args.lon_end,
+         args.z_start,
+         args.z_end,
+         args.modelname,
+         args.modelpath)
 
-    write_csv_output(df, f'2D_vertical_cross_section_{modelname}.csv', '2D_vertical', modelname)
+    # Call the function to write the output csv file
+    write_csv_output(df, args.outpath, '2D_vertical', args.modelname)
+
+# Make sure the following is not calling when it is being imported, only runs the following when directly run at command prompt
+if __name__ == "__main__":
+    # Call the query function
+    call_func()
+
