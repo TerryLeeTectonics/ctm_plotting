7ba0336736b57aa5410b61c84e53d302c8ee8c8a
TerryLeeTectonics <terrywaiholee@gmail.com>
2026-01-23 12:48:13 -0800

    Update Initiation.py
    Added initiation code for Sui et al 2025 and Boyd 2019 models.

--- pyctm/Initiation.py
+++ pyctm/Initiation.py
@@ -1,4 +1,3 @@
-
 ### Import Packages
 import xarray as xr
 
@@ -8,28 +7,60 @@ import xarray as xr
 def init_ctm(modelname, modelpath):
     
     # open dataset
-    xdata = xr.open_dataset(modelpath, engine="netcdf4")
+    xdata = xr.open_dataset(modelpath)
 
     if modelname == 'Lee_2025':
         # configure: change unit to meters and rename all variables
-        xdata = xdata.assign_coords({"depth": xdata.depth*1000.0})
+        xdata = xdata.assign_coords({"depth": xdata.depth * 1000.0})
         xdata = xdata.rename_dims({"longitude": "longitude[°]", 
                                    "latitude": "latitude[°]",
                                    "depth": "depth[m]"})
+        
         xdata = xdata.rename_vars({"longitude": "longitude[°]", 
                                    "latitude": "latitude[°]",
                                    "depth": "depth[m]",
                                    "temperature_diffused": "temperature[°C]"})
+
+        xdata['depth[m]'] = xdata['depth[m]'] * 1000
 
     elif modelname == 'Shinevar_2018':
         # configure: rename all variables
         xdata = xdata.rename_dims({"longitude": "longitude[°]", 
                                    "latitude": "latitude[°]",
                                    "depth": "depth[m]"})
+        
         xdata = xdata.rename_vars({"longitude": "longitude[°]", 
                                    "latitude": "latitude[°]",
                                    "depth": "depth[m]",
                                    "temperature": "temperature[°C]"})
+
+    elif modelname == 'Boyd_2019':
+        # configure: rename all variables
+        xdata = xdata.rename_dims({"dimk": "longitude[°]", 
+                                   "dimj": "latitude[°]",
+                                   "diml": "depth[m]"})
+        
+        xdata = xdata.rename_vars({"Longitude": "longitude[°]", 
+                                   "Latitude": "latitude[°]",
+                                   "Depth": "depth[m]",
+                                   "Temperature": "temperature[°C]"})
+        
+        xdata = xdata.set_coords(['longitude[°]', 'latitude[°]', 'depth[m]'])
+
+    elif modelname == 'Suietal_2025':
+        # configure: rename all variables
+        xdata = xdata.rename_dims({'dimk': 'longitude[°]', 
+                                   'dimj': 'latitude[°]',
+                                   'diml': 'depth[m]'})
+        
+        xdata = xdata.rename_vars({'Longitude': 'longitude[°]', 
+                                   'Latitude': 'latitude[°]',
+                                   'Depth': 'depth[m]',
+                                   'Temperature': 'temperature[°C]'})
+        
+        xdata = xdata.set_coords(['longitude[°]', 'latitude[°]', 'depth[m]'])
+        xdata = xdata.assign_coords({'depth[m]': xdata['depth[m]'] * 1000.0})
+        
         
     # return
     return xdata
